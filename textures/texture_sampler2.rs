30131d64250783a41421801e644f76e36b2f4e36
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-10-24 22:30:48 -0700

    :sparkles: texture experiments

---
+++ textures/texture_sampler2.rs
@@ -0,0 +1,63 @@
+//! A simple as possible example demonstrating how to use the `draw` API to display a texture.
+
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).run();
+}
+
+struct Model {
+    texture: wgpu::Texture,
+}
+
+fn model(app: &App) -> Model {
+    // Create a new window! Store the ID so we can refer to it later.
+    app.new_window().size(512, 512).view(view).build().unwrap();
+    // Load the image from disk and upload it to a GPU texture.
+    let assets = app.assets_path().unwrap();
+    let img_path = assets.join("avatar.jpg");
+    let texture = wgpu::Texture::from_path(app, img_path).unwrap();
+    Model { texture }
+}
+
+// Draw the state of your `Model` into the given `Frame` here.
+fn view(app: &App, model: &Model, frame: Frame) {
+    frame.clear(BLACK);
+    let win = app.main_window();
+    let win_r = win.rect();
+
+    // Let's choose the address mode based on the mouse position.
+    let address_mode = wgpu::AddressMode::Repeat;
+
+    // Create a sampler with the chosen address mode.
+    let sampler = wgpu::SamplerBuilder::new()
+        .address_mode(address_mode)
+        .into_descriptor();
+
+    // At any point during drawing, we can create a new `draw` context that will let us use a
+    // different sampler.
+    let draw = app.draw();
+    let draw = draw.sampler(sampler);
+
+    // Change the texture coordinates to sample outside the texture. This will demonstrate how the
+    // sampler behaves when sampling beyond the bounds of the texture in each of the different
+    // address modes. By default, the bounds of the texture coordinates are 0.0 to 1.0. We will
+    // triple the size.
+    let area = geom::Rect::from_x_y_w_h(
+        app.time.sin() * 0.5,
+        app.time.cos() * 0.5,
+        app.time.cos() * 5.0,
+        app.time.cos() * 5.0,
+    );
+
+    draw.texture(&model.texture).area(area);
+
+    // Draw the current address mode in the bottom left corner.
+    // let text = format!("Address mode: {:?}", address_mode);
+    // draw.text(&text)
+    //     .wh(win_r.wh() * 0.95)
+    //     .left_justify()
+    //     .align_text_bottom();
+
+    draw.to_frame(app, &frame).unwrap();
+}
